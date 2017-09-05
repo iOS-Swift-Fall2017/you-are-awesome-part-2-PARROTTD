5d07bc45f5450ed8b424e6dc3a5131d50f6a9ee4
Duncan Parrott <duncanparrott@Duncans-MacBook-Pro-2.local>
2017-09-05 19:52:23 -0400

    Finished through 2.11

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
+    // this code executes when the view controller loads
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        print("The View Controller Loaded")
     }
 
 
-}
+    @IBAction func MessageButtonPressed(_ sender: UIButton) {
+        
+        let messages = ["You are fantastic",
+                       "You are Great",
+                       "You are Amazing",
+                       "When the Genius Bar needs help, they call you",
+                       "You brighton my day",
+                       "You are da bomb",
+                       "I can't wait to use your app"]
+        
+        var newIndex = -1
+        
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        }while index == newIndex
+        
+        index = newIndex
+        messageLabel.text = messages[index]
+        
+        /*messageLabel.text = message[index]
+        index = index + 1
+        
+        if index == message.count {
+            index = 0 */
+        
+        /* let message1 = "You are fantastic"
+        let message2 = "You are great"
+        let message3 = You are amazing"
+ 
+ messageLabel.text == message1{
+ messageLabel.text = message2
+ } else if messageLabel.text == message2{
+ messageLabel.text = message3
+         }else messageLabel.text = message1}*/
+        }
+        
+    }
+    
+
 
